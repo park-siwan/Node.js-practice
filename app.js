97e836bd57a3aade98046d77f62fcbf09a5f34c4
park-siwan <pswan3168@gmail.com>
2022-05-16 13:24:50 +0900

    express 사용해보기, express type설치

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello Express</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening...');
+});
